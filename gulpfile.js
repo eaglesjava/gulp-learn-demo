0e668f44a61c96986053e48e8ad65d1e411dae00
scott <1028282161@qq.com>
2015-07-26 09:35:36 +0800

    复制单个文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 
-gulp.task('hello',function(){
+//1、创建任务
+
+/*gulp.task('hello',function(){
 	console.log('您好');
 });
 
 
-gulp.task('default',['hello']);
+gulp.task('default',['hello']);*/
 
+//2、复制单个文件
+
+gulp.task('copy-index',function(){
+	return gulp.src('index.html').pipe(gulp.dest('dist'));
+});
