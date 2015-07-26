6fac580e4d506ce5bb5f35b43bf8a91c76480f40
scott <1028282161@qq.com>
2015-07-26 10:19:05 +0800

    globs

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,17 @@ gulp.task('copy-index',function(){
 	return gulp.src('index.html').pipe(gulp.dest('dist'));
 });
 
-//3、复制多个文件
+//3、复制多个文件 | globs
 gulp.task('img',function(){
-	return gulp.src('img/*.jpg').pipe(gulp.dest('dist/img'));
+	//复制多个文件
+	//return gulp.src('img/*.jpg').pipe(gulp.dest('dist/img'));
+	
+	//globs
+	//return gulp.src('img/*.{jpg,png}').pipe(gulp.dest('dist/img'));
+	//return gulp.src('img/*').pipe(gulp.dest('dist/img'));
+	//return gulp.src('img/*/*').pipe(gulp.dest('dist/img'));
+	return gulp.src('img/**/*').pipe(gulp.dest('dist/img'));
+	
 });
 
 
