f374fcaba99c0ff2f40077a99c28421ed6cb5e2e
scott <1028282161@qq.com>
2015-07-26 10:02:53 +0800

    复制多个文件

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,17 @@ var gulp = require('gulp');
 gulp.task('default',['hello']);*/
 
 //2、复制单个文件
-
 gulp.task('copy-index',function(){
 	return gulp.src('index.html').pipe(gulp.dest('dist'));
 });
+
+//3、复制多个文件
+gulp.task('img',function(){
+	return gulp.src('img/*.jpg').pipe(gulp.dest('dist/img'));
+});
+
+
+
+
+
+
